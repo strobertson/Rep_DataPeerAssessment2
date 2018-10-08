8f54fdd38d1ef7e4a266e48945dc2939a52597b8
Scott Robertson <scottrobertson1066@gmail.com>
2018-10-08 14:12:38 +0100

    Saving update of work on code on 8 October

--- working_code.r
+++ working_code.r
@@ -9,8 +9,10 @@ if (!file.exists("data")) {
 url <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2"
 
 # Download data file to data folder, if not already present, and store download time
-if(!file.exists("./data/stormdata.csv")) {
-        download.file(fileUrl, "./data/stormdata.csv", method = "curl")
+if(!file.exists("./data/stormdata.csv.bz2")) {
+        download.file(url, "./data/stormdata.csv.bz2", method = "curl")
         date_downloaded <- Sys.time()
 }
 
+# Read the csv file into environment
+storm_data <- read.csv("./data/stormdata.csv.bz2", header = TRUE, sep = ",")
